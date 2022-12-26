94b8ce40f439f8dc46dc603fb5a05a2644063636
Jason Tjahjono <jasontjahjono.webdev@gmail.com>
2022-12-26 14:02:59 +0700

    Connect Registration Form with Firebase

---
+++ src/js/api/auth.js
@@ -0,0 +1,24 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import db from "../db/firestore";
+import { setDoc, doc } from "firebase/firestore";
+
+const createUserProfile = async (userProfile) => {
+  await setDoc(doc(db, "profiles", userProfile.uid), userProfile);
+};
+
+export const register = async ({ email, password, username, avatar }) => {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(({ user }) => {
+      createUserProfile({
+        uid: user.uid,
+        username,
+        email,
+        avatar,
+        joinedChats: [],
+      });
+    })
+    .catch((err) => {
+      Promise.reject(err.message);
+    });
+};
