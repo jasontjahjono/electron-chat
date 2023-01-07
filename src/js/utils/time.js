922d2abebffed4478e44b8b6052c164a9085d7ef
Jason Tjahjono <jasontjahjono.webdev@gmail.com>
2023-01-07 16:15:17 +0700

    Move Author's Chat Bubbles to the Right and Format Timestamp

--- src/js/utils/time.js
+++ src/js/utils/time.js
@@ -1,3 +1,7 @@
 import { timestamp } from "../db/firestore";
+import moment from "moment";
 
 export const createTimestamp = () => timestamp.now().toMillis().toString();
+
+export const formatTimeAgo = (timestamp) =>
+  moment(parseInt(timestamp, 10)).fromNow();
