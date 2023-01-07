87b2e684c55081194df0bc5b62f3a9dd82321842
Jason Tjahjono <jasontjahjono.webdev@gmail.com>
2023-01-07 19:45:51 +0700

    Cleanup State After Logout and Unsubscribe from Messages

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
 import authReducer from "../reducers/auth";
 import chatReducer from "../reducers/chats";
@@ -6,12 +6,22 @@ import appReducer from "../reducers/app";
 
 import appMiddleware from "./middlewares/app";
 
+const mainReducer = combineReducers({
+  chats: chatReducer,
+  auth: authReducer,
+  app: appReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === "AUTH_LOGOUT_SUCCESS") {
+    state = undefined;
+  }
+
+  return mainReducer(state, action);
+};
+
 const store = configureStore({
-  reducer: {
-    chats: chatReducer,
-    auth: authReducer,
-    app: appReducer,
-  },
+  reducer: rootReducer,
   middleware: [thunkMiddleware, appMiddleware],
 });
 
